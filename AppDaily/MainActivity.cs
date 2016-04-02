bd58b6bb5ae0732afe46fe2e0d0595d42b3f51bc
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-04-02 01:05:56 -0500

    Set up basic current data file checking

--- AppDaily/MainActivity.cs
+++ AppDaily/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using System.IO;
+
 namespace AppDaily
 {
 	[Activity(Label = "AppDaily", MainLauncher = true, Icon = "@drawable/icon")]
@@ -25,6 +27,23 @@ namespace AppDaily
 			Button button = FindViewById<Button>(Resource.Id.MyButton);
 
 			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+			// first see if we can find the data files 
+			//Context context = this.ApplicationContext;
+
+			string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+			Directory.SetCurrentDirectory(documentsPath);
+
+			//button.Text = Directory.GetCurrentDirectory(); 
+			if (!File.Exists("./CurrentData.dat"))
+			{
+				button.Text = "Nothing found yet";
+				File.Create("./CurrentData.dat");
+			}
+			else
+			{
+				button.Text = "Found the data!";
+			}
 		}
 	}
 }
